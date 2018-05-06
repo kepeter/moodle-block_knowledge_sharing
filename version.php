15bfdb1d9d17ee846f2cdfeaa26e5970cacfde4d
kepeter <kepeter@gmail.com>
2018-05-06 16:32:32 +0300

    Update version format as requested by Moodle

--- version.php
+++ version.php
@@ -1,7 +1,7 @@
 <?php
 defined ( 'MOODLE_INTERNAL' ) || die ();
 
-$plugin->version = 2017032600.1615;
+$plugin->version = 2017032600;
 $plugin->release = '1.0.0.1';
 $plugin->requires = 2016052300;
 $plugin->component = 'block_knowledge_sharing';
