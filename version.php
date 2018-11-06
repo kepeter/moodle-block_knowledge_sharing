1cb9253907bb322f96dc849738221e0603cdb107
Peter Eliyahu Kornfeld <kepeter@gmail.com>
2018-11-06 16:51:32 +0200

    Set requested MOODLE version to 3.5

--- version.php
+++ version.php
@@ -17,6 +17,6 @@ defined ( 'MOODLE_INTERNAL' ) || die ();
 
 $plugin->version = 2018100300;
 $plugin->release = '1.0.0.1';
-$plugin->requires = 2016052300;
+$plugin->requires = 2018051700;
 $plugin->component = 'block_knowledge_sharing';
 $plugin->maturity = MATURITY_STABLE;
