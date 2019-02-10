f415177a57ee2c887c7222b938cc13e4f1a47dcb
Peter Eliyahu Kornfeld <kepeter@gmail.com>
2019-02-10 15:36:28 +0200

    Backport to 3.4 STABLE

--- version.php
+++ version.php
@@ -25,6 +25,6 @@ defined ( 'MOODLE_INTERNAL' ) || die ();
 
 $plugin->version = 2019021000;
 $plugin->release = '1.0.0.3';
-$plugin->requires = 2018051700;
+$plugin->requires = 2017111300;
 $plugin->component = 'block_knowledge_sharing';
 $plugin->maturity = MATURITY_STABLE;
