0db1ffbe29a8c5f08effd9da1e420122f53281d9
YahakovG <66909487+YahakovG@users.noreply.github.com>
2021-06-10 16:54:48 +0300

    fix signature that crash the cron on data request

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -27,7 +27,7 @@ defined ( 'MOODLE_INTERNAL' ) || die ();
 
 class provider implements \core_privacy\local\metadata\null_provider {
 
-    public static function get_reason() {
+    public static function get_reason() : string {
         return 'privacy:metadata';
     }
 }
